da310de348919650661285de544a4813e545044d
GilianS <r0885351@student.thomasmore.be>
2023-03-16 12:32:44 +0100

    style: proper content formatting

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,16 @@ import Navigation from './components/Navigation.vue';
     </div>
 </template>
 
-<style scoped>
+<style>
+#app {
+    display: grid;
+    grid-template-columns: 220px 1fr;
+}
+#content {
+    padding: 1rem 3rem;
+    height: calc(100vh);
+    overflow-y: auto;
+    background-color: var(--background);
+    border-radius: 2rem 0 0 2rem;
+}
 </style>
