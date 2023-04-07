3fbf59656e276e45d66631abf673266903d85b79
GilianS <r0885351@student.thomasmore.be>
2023-04-07 10:52:51 +0200

    feat: 2nd card on homescreen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,6 +21,25 @@ import { RouterLink } from 'vue-router';
             </div>
         </template>
     </Card>
+    <Card class="p-m-4" style="width: 360px">
+        <template #header>
+            <img src="../assets/images/senior_man_working@w360.webp" alt="Senior group working in a garden">
+        </template>
+        <template #title>Ik ga helpen!</template>
+        <template #content>
+            <p style="margin-top: 0px">
+                Wat fijn dat jij een van onze klussers wil worden! We hebben klusjes in verschillende sectoren, neem snel
+                een kijkje!
+            </p>
+        </template>
+        <template #footer>
+            <div class="flex justify-content-around">
+                <RouterLink to="/jobs">
+                    <Button label="Zoek een klusje" />
+                </RouterLink>
+            </div>
+        </template>
+    </Card>
 </template>
 
 <style></style>
