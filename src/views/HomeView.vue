330af55b15b266eb6a93f36c66c678e4cec32fc4
GilianS <r0885351@student.thomasmore.be>
2023-04-07 11:15:35 +0200

    fix: navigation button returned on home screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,44 +2,48 @@
 import { RouterLink } from 'vue-router';
 </script>
 <template>
-    <Card class="p-m-4" style="width: 360px">
-        <template #header>
-            <img src="../assets/images/senior_group_gardening@w360.webp" alt="Senior group working in a garden">
-        </template>
-        <template #title>Hulp nodig?</template>
-        <template #content>
-            <p style="margin-top: 0px">
-                Onze klussers staan paraat! Door een klusje te plaatsen, kan je meteen zoeken naar de klusser die jij nodig
-                hebt.
-            </p>
-        </template>
-        <template #footer>
-            <div class="flex justify-content-around">
-                <RouterLink to="/jobs">
+    <div class="w-full h-full flex justify-content-center align-items-center">
+        <Card class="p-m-4 mx-6" style="width: 360px">
+            <template #header>
+                <img height="200" class="img-fit" src="../assets/images/senior_group_gardening@w360.webp"
+                    alt="Senior group working in a garden">
+            </template>
+            <template #title>Hulp nodig?</template>
+            <template #content>
+                <p style="margin-top: 0px">
+                    Onze klussers staan paraat! Door een klusje te plaatsen, kan je meteen zoeken naar de klusser die jij
+                    nodig
+                    hebt.
+                </p>
+            </template>
+            <template #footer>
+                <div class="flex justify-content-around pb-4">
                     <Button label="Plaats een klusje" />
-                </RouterLink>
-            </div>
-        </template>
-    </Card>
-    <Card class="p-m-4" style="width: 360px">
-        <template #header>
-            <img src="../assets/images/senior_man_working@w360.webp" alt="Senior group working in a garden">
-        </template>
-        <template #title>Ik ga helpen!</template>
-        <template #content>
-            <p style="margin-top: 0px">
-                Wat fijn dat jij een van onze klussers wil worden! We hebben klusjes in verschillende sectoren, neem snel
-                een kijkje!
-            </p>
-        </template>
-        <template #footer>
-            <div class="flex justify-content-around">
-                <RouterLink to="/jobs">
-                    <Button label="Zoek een klusje" />
-                </RouterLink>
-            </div>
-        </template>
-    </Card>
+                </div>
+            </template>
+        </Card>
+        <Card class="p-m-4 mx-6" style="width: 360px">
+            <template #header>
+                <img height="200" class="img-fit" src="../assets/images/senior_man_working@w360.webp"
+                    alt="Senior group working in a garden">
+            </template>
+            <template #title>Ik ga helpen!</template>
+            <template #content>
+                <p style="margin-top: 0px">
+                    Wat fijn dat jij een van onze klussers wil worden! We hebben klusjes in verschillende sectoren, neem
+                    snel
+                    een kijkje!
+                </p>
+            </template>
+            <template #footer>
+                <div class="flex justify-content-around pb-4">
+                    <RouterLink to="/jobs">
+                        <Button label="Zoek een klusje" />
+                    </RouterLink>
+                </div>
+            </template>
+        </Card>
+    </div>
 </template>
 
 <style></style>
