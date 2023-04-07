1321c532bf6d7c0507cff34528cb6f40d6cfecd3
GilianS <r0885351@student.thomasmore.be>
2023-04-07 10:48:10 +0200

    feat: show card to create job on homescreen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 <template>
-    <Card class="p-m-4">
-        <template #title>Home</template>
+    <Card class="p-m-4" style="width: 360px">
+        <template #header>
+            <img src="../assets/images/senior_group_gardening@w360.webp" alt="Senior group working in a garden">
+        </template>
+        <template #title>Hulp nodig?</template>
         <template #content>
+            <p style="margin-top: 0px">
+                Onze klussers staan paraat! Door een klusje te plaatsen, kan je meteen zoeken naar de klusser die jij nodig
+                hebt.
+            </p>
+        </template>
+        <template #footer>
+            <div class="flex justify-content-around">
+                <RouterLink to="/jobs">
+                    <Button label="Plaats een klusje" />
+                </RouterLink>
+            </div>
         </template>
     </Card>
 </template>
+
+<style></style>
