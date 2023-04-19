8ba9376ac9bc3c786eb5ce69952f6d89ac2a8503
Ladis Nelissen <ladisnelissen@hotmail.com>
2023-04-19 14:22:54 +0200

    feat: generate and style tiles from JobTile component in JobView

---
+++ src/components/JobTile.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="job-tile" :style="{ width: tileWidth + 'px' }">
+      <div class="image-with-details" :style="{ width: imageWidth + 'px', height: imageWidth + 'px', borderRadius: borderRadius + 'px' }">
+        <div class="image-container">
+          <img :src="job.image" alt="Job Image" :style="{ borderRadius: borderRadius + 'px' }" />
+        </div>
+        <div class="details-container">
+          <div class="details-inner">
+            <h1>{{ job.name }}</h1>
+            <h3>{{ job.description }}</h3>
+            <div class="price-and-button">
+              <p class="price">€{{ job.price }}/ u</p>
+              <button>Details</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      job: {
+        type: Object,
+        required: true
+      },
+      tileWidth: {
+        type: Number,
+        default: 300 // default width for the tile
+      },
+      imageWidth: {
+        type: Number,
+        default: 280 // default width for the image
+      },
+      borderRadius: {
+        type: Number,
+        default: 10 // default border radius for the tile and image
+      }
+    }
+  };
+  </script>
+  <style>
+  .job-tile {
+    display: inline-block;
+    margin-right: 10px;
+    margin-top: 10px;
+  }
+  
+  .image-with-details {
+    position: relative;
+    overflow: hidden;
+    text-align: left;
+    margin-bottom: 10px;
+}
+
+  
+  .image-container {
+    display: inline-block;
+    vertical-align: middle; /* Vertically center the image */
+  }
+  
+  .image-container img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  
+  .details-container {
+    position: absolute;
+    bottom: 0;
+    left: 50%; /* Horizontally center the details */
+    transform: translate(-50%, 100%); /* Move the details below the image */
+    width: 100%;
+    background-color: #fff;
+    padding: 20px;
+    box-sizing: border-box;
+    text-align: center;
+    transition: transform 0.3s ease-in-out;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1) inset;
+  }
+  
+  .job-tile:hover .details-container {
+    transform: translate(-50%, 0); /* Move the details up */
+  }
+  
+  .details-inner {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 100%;
+  }
+  
+  .details-inner h1,
+  .details-inner h3,
+  .details-inner p {
+    margin-top: 0;
+    margin-bottom: 10px;
+    text-align: left;
+  }
+  
+  .details-inner .price {
+    margin-top: 20px;
+    margin-bottom: 0;
+    font-weight: bold;
+  }
+  
+  .details-inner .price-and-button {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  
+  .price-and-button .price {
+    margin-top: 0;
+    color: #689F38;
+    font-size: 1.2rem;
+  }
+  
+  .details-inner button {
+    border-radius: 30px;
+    color: #255586;
+    background-color: #fff;
+    border: 2px solid #255586;
+    padding: 10px 20px;
+    font-size: 1.2rem;
+  }
+  
+  .details-inner button:hover {
+    background-color: #255586;
+    color: #fff;
+    border-color: #fff;
+    cursor: pointer;
+  }
+</style>
--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,8 +1,51 @@
 <template>
     <Card class="p-m-4 p-major">
-        <template #title>Mijn Klusjes</template>
-        <template #content>
-
-        </template>
+      <template #title>Mijn Klusjes</template>
+      <template #content>
+        <InputText v-model="searchTerm" placeholder="Zoek een klusje" />
+        <div v-if="jobs.length" class="job-tiles-container">
+          <div v-for="job in jobs" :key="job._id">
+            <JobTile :job="job" />
+          </div>
+        </div>
+        <div v-else>
+          Loading...
+        </div>
+      </template>
     </Card>
-</template>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import JobTile from '../components/JobTile.vue';
+  
+  const searchTerm = ref('');
+  const jobs = ref([]);
+  
+  // Fetch jobs from the API
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch('http://localhost:27017/api/v1/klusje/');
+      const { data } = await response.json();
+      jobs.value = data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+  onMounted(fetchJobs);
+  </script>
+
+<style>
+.job-tiles-container {
+    margin-top: 30px;
+    display: flex;
+    flex-wrap: wrap;
+}
+</style>
+
+
+
+
+
+
