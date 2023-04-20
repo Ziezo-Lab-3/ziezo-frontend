93ffe7930c65ee17932501ef0ac8673ef504aafd
GilianS <r0885351@student.thomasmore.be>
2023-04-20 12:13:48 +0200

    feat: disable button until requirements are met

--- src/components/DialogNewJob.vue
+++ src/components/DialogNewJob.vue
@@ -1,6 +1,6 @@
 <script setup>
 import FileUpload from 'primevue/fileupload';
-import { ref, reactive, watch, defineEmits } from 'vue';
+import { ref, reactive, watch, defineEmits, computed } from 'vue';
 const visible = ref(false);
 const emit = defineEmits(['close']);
 const props = defineProps({ visible: false });
@@ -8,17 +8,21 @@ watch(() => props.visible, (val) => visible.value = val);
 
 const close = () => {
     props.visible = false;
+    state.description = '';
+    state.title = '';
+    state.price = null;
+    state.images = [];
     emit('close');
 }
 
 const accept = () => {
     console.log(state);
-    state.description = '';
-    state.title = '';
-    state.price = null;
-    state.images = [];
     close();
 }
+
+const canPost = computed(() => {
+    return state.title.length > 0 && state.description.length > 24 && state.price > 5;
+});
 
 const state = reactive({
     title: '',
@@ -31,15 +35,15 @@ const state = reactive({
     <Dialog :visible="visible" :closable="false" header="Klusje Plaatsen" modal :style="{width: '540px'}" :breakpoints="{  '580px': 'calc(100vw - 1rem)' }">
         <div class="labelInput">
             <label>Titel</label>
-            <InputText v-model="state.title" placeholder="Kleerkast assembleren" />
+            <InputText v-model="state.title" placeholder="Kleerkast assembleren" :required="true"/>
         </div>
         <div class="labelInput">
             <label>Beschrijving</label>
-            <TextArea v-model="state.description" placeholder="Ik heb hulp nodig met..." autoResize style="resize: none" />
+            <TextArea v-model="state.description" placeholder="Ik heb hulp nodig met..." autoResize style="resize: none" :required="true" :minlength="24" />
         </div>
         <div class="labelInput">
             <label>Prijs</label>
-            <InputNumber v-model="state.price" mode="currency" currency="EUR" placeholder="€15,00" :minFractionDigits="2" :maxFractionDigits="2" />
+            <InputNumber v-model="state.price" mode="currency" currency="EUR" placeholder="€15,00" :minFractionDigits="2" :maxFractionDigits="2" :min="5" :required="true"/>
         </div>
         <div class="labelInput">
             <label>Afbeeldingen</label>
@@ -52,7 +56,7 @@ const state = reactive({
         <template #footer>
             <div class="flex justify-content-between" >
                 <Button label="Cancel" @click="close" class="p-button-secondary" />
-                <Button label="Plaats Klusje" @click="accept" />
+                <Button label="Plaats Klusje" @click="accept" :disabled="!canPost" />
             </div>
         </template>
     </Dialog>
