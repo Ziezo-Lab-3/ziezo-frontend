089a19ea83c1c311dbcb39a122c4a3c82be16cc9
GilianS <r0885351@student.thomasmore.be>
2023-04-20 11:27:07 +0200

    feat: added content to dialog + open/close dialog

--- src/components/DialogNewJob.vue
+++ src/components/DialogNewJob.vue
@@ -1,18 +1,58 @@
 <script setup>
-import { ref } from 'vue';
+import FileUpload from 'primevue/fileupload';
+import { ref, reactive, watch, defineEmits } from 'vue';
 const visible = ref(false);
+const emit = defineEmits(['close']);
+const props = defineProps({ visible: false });
+watch(() => props.visible, (val) => visible.value = val);
 
-const toggle = () => {
-    visible.value = !visible.value;
+const close = () => {
+    props.visible = false;
+    emit('close');
 }
+
+const accept = () => {
+    console.log(state);
+    state.description = '';
+    state.title = '';
+    state.price = null;
+    state.images = [];
+    close();
+}
+
+const state = reactive({
+    title: '',
+    description: '',
+    price: null,
+    images: []
+});
 </script>
 <template>
-    <Dialog :closable="false" header="Klusje Plaatsen" :visible="visible" modal width="50vw" :breakpoints="{  '960px': '75vw', '641px': 'calc(100vw - 1rem)' }">
-        <p>Test</p>
+    <Dialog :visible="visible" :closable="false" header="Klusje Plaatsen" modal :style="{width: '540px'}" :breakpoints="{  '580px': 'calc(100vw - 1rem)' }">
+        <div class="labelInput">
+            <label>Titel</label>
+            <InputText v-model="state.title" placeholder="Kleerkast assembleren" />
+        </div>
+        <div class="labelInput">
+            <label>Beschrijving</label>
+            <TextArea v-model="state.description" placeholder="Ik heb hulp nodig met..." autoResize style="resize: none" />
+        </div>
+        <div class="labelInput">
+            <label>Prijs</label>
+            <InputNumber v-model="state.price" mode="currency" currency="EUR" placeholder="€15,00" :minFractionDigits="2" :maxFractionDigits="2" />
+        </div>
+        <div class="labelInput">
+            <label>Afbeeldingen</label>
+            <FileUpload :disabled="true" name="images[]" mode="advanced" accept="image/*" maxFileSize="1000000" :multiple="true" >
+                <template #empty>
+                    <p>Sleep afbeeldingen naar hier om te uploaden.</p>
+                </template>
+            </FileUpload>
+        </div>
         <template #footer>
             <div class="flex justify-content-between" >
-                <Button label="Cancel" @click="toggle" class="p-button-secondary" />
-                <Button label="Plaats Klusje" @click="toggle" />
+                <Button label="Cancel" @click="close" class="p-button-secondary" />
+                <Button label="Plaats Klusje" @click="accept" />
             </div>
         </template>
     </Dialog>
