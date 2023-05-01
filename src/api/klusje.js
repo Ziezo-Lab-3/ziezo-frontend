a74e122ed171b0c43fa3869407bd7183eb601b29
GilianS <r0885351@student.thomasmore.be>
2023-05-01 18:49:07 +0200

    feat: lazy call to backend

--- src/api/klusje.js
+++ src/api/klusje.js
@@ -1,7 +1,12 @@
 const URL = import.meta.env.VITE_BACKEND_URL;
 
+/**
+ * 
+ * @param { { name: string, description: string, category: string, price: number, image: string, address: string } } klusje 
+ * @param { string } token 
+ * @returns { { _id: string, name: string, description: string, price: number, image: string, createdAt: string, updatedAt: string, __v: number } }
+ */
 export const postKlusje = async (klusje, token) => {
-    console.log(klusje);
     const response = await fetch(`${URL}/klusje`, {
         method: "POST",
         headers: {
@@ -11,9 +16,24 @@ export const postKlusje = async (klusje, token) => {
         body: JSON.stringify(klusje),
     });
     return await response.json();
-}
+};
 
-export const getKlusjes = async (token) => {
-    const response = await fetch(`${URL}/klusjes`);
+/**
+ * 
+ * @param { string } token 
+ * @param { { first: number, last: number, filter: string} } params 
+ * @returns { { status: string, message: string, data: [{ _id: string, name: string, description: string, price: number, image: string, createdAt: string, updatedAt: string, __v: number }]}}
+ */
+export const getKlusjes = async (token, params) => {
+    let suffix = "";
+    if (params) {
+        //Get all the keys and values from the filter object, and append them to the url.
+        for (const [key, value] of Object.entries(params)) {
+            suffix += `${key}=${value}&`;
+        }
+        //Remove the last & from the url.
+        suffix = suffix.slice(0, -1);
+    }
+    const response = await fetch(`${URL}/klusje?${suffix}`);
     return await response.json();
-}
+};
--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import moment from 'moment';
 import { ref } from 'vue';
+import decodeJWT from '../js/decodeJWT';
+import { getKlusjes } from '../api/klusje';
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
 import DataTable from 'primevue/datatable';
@@ -8,87 +10,28 @@ import Column from 'primevue/column';
 import Tag from 'primevue/tag';
 
 const getSeverity = (status) => {
-    console.log(status);
     if (status == "Klaar") return "success";
     else if (status == "Bezig") return "warning";
     else if (status == "Niet klaar") return "danger";
     else return "primary";
 }
 
-const jobs = ref([
-    {
-        _id: "644120534474c4b01d8804f8",
-        name: "Apple bottom jeans",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at elit sit amet purus porta eleifend facilisis non neque. Mauris hendrerit sagittis diam, quis condimentum ante porta eu. Sed vitae velit ut elit maximus pharetra ac quis erat. Nam ipsum mauris, egestas sodales est a, semper volutpat nisi. Ut lacus dui, porta a tincidunt posuere, tempor quis dui. Nulla fringilla sagittis nisl a placerat. Nunc id nibh sit amet ligula pulvinar convallis.",
-        category: "Handwerk",
-        status: "Niet klaar",
-        price: 30,
-        image: "https://loremflickr.com/400/300/apple,bottom,jeans",
-        createdAt: "2023-04-20T11:21:55.251Z",
-        updatedAt: "2023-04-20T11:21:55.251Z",
-        __v: 0
-    },
-    {
-        _id: "644120534474c4b01d8804f8",
-        name: "Boots with the fur",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at elit sit amet purus porta eleifend facilisis non neque. Mauris hendrerit sagittis diam, quis condimentum ante porta eu. Sed vitae velit ut elit maximus pharetra ac quis erat. Nam ipsum mauris, egestas sodales est a, semper volutpat nisi. Ut lacus dui, porta a tincidunt posuere, tempor quis dui. Nulla fringilla sagittis nisl a placerat. Nunc id nibh sit amet ligula pulvinar convallis.",
-        category: "Huishouden",
-        status: "Niet klaar",
-        price: 345.62,
-        image: "https://loremflickr.com/400/300/boots,fur",
-        createdAt: "2023-04-20T11:21:55.251Z",
-        updatedAt: "2023-04-20T11:21:55.251Z",
-        __v: 0
-    },
-    {
-        _id: "644120534474c4b01d8804f8",
-        name: "The whole club is looking at her",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at elit sit amet purus porta eleifend facilisis non neque. Mauris hendrerit sagittis diam, quis condimentum ante porta eu. Sed vitae velit ut elit maximus pharetra ac quis erat. Nam ipsum mauris, egestas sodales est a, semper volutpat nisi. Ut lacus dui, porta a tincidunt posuere, tempor quis dui. Nulla fringilla sagittis nisl a placerat. Nunc id nibh sit amet ligula pulvinar convallis.",
-        category: "Electriciteit",
-        status: "Niet klaar",
-        price: 120,
-        image: "https://loremflickr.com/400/300/club,looking",
-        createdAt: "2023-04-20T11:21:55.251Z",
-        updatedAt: "2023-04-20T11:21:55.251Z",
-        __v: 0
-    },
-    {
-        _id: "644120534474c4b01d8804f8",
-        name: "She hit the floor",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at elit sit amet purus porta eleifend facilisis non neque. Mauris hendrerit sagittis diam, quis condimentum ante porta eu. Sed vitae velit ut elit maximus pharetra ac quis erat. Nam ipsum mauris, egestas sodales est a, semper volutpat nisi. Ut lacus dui, porta a tincidunt posuere, tempor quis dui. Nulla fringilla sagittis nisl a placerat. Nunc id nibh sit amet ligula pulvinar convallis.",
-        category: "Constructie",
-        status: "Bezig",
-        price: 22,
-        image: "https://loremflickr.com/400/300/floor",
-        createdAt: "2023-04-20T11:21:55.251Z",
-        updatedAt: "2023-04-20T11:21:55.251Z",
-        __v: 0
-    },
-    {
-        _id: "644120534474c4b01d8804f8",
-        name: "Next thing you know",
-        description: "Verhuizen",
-        status: "Klaar",
-        category: "Handwerk",
-        price: 35,
-        image: "https://loremflickr.com/400/300/thing,know",
-        createdAt: "2023-04-20T11:21:55.251Z",
-        updatedAt: "2023-04-20T11:21:55.251Z",
-        __v: 0
-    },
-    {
-        _id: "644120534474c4b01d8804f8",
-        name: "Shawty got low low low low low low low low",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at elit sit amet purus porta eleifend facilisis non neque. Mauris hendrerit sagittis diam, quis condimentum ante porta eu. Sed vitae velit ut elit maximus pharetra ac quis erat. Nam ipsum mauris, egestas sodales est a, semper volutpat nisi. Ut lacus dui, porta a tincidunt posuere, tempor quis dui. Nulla fringilla sagittis nisl a placerat. Nunc id nibh sit amet ligula pulvinar convallis.",
-        category: "Oppassen",
-        status: "Klaar",
-        price: 12.50,
-        image: "https://loremflickr.com/400/300/shawty,low",
-        createdAt: "2023-04-20T11:21:55.251Z",
-        updatedAt: "2023-04-20T11:21:55.251Z",
-        __v: 0
-    }
-]);
+const data = ref([]);
+const isLoadingData = ref(false);
+const onLazyLoad = async (event) => {
+    const decoded = decodeJWT(localStorage.getItem('token'));
+    isLoadingData.value = true;
+    const lazyData = await getKlusjes(localStorage.getItem('token'), {
+        first: event.first,
+        last: event.last,
+        filter: JSON.stringify({
+            _id: decoded.id,
+        }),
+    });
+    data.value.push(...lazyData.data);
+    isLoadingData.value = false;
+}
+
 </script>
 <template>
     <Card class="p-m-4 p-major">
@@ -96,13 +39,20 @@ const jobs = ref([
         <template #content>
             <TabView>
                 <TabPanel header="Zelf Geplaatste Klusjes">
-                    <DataTable :value="jobs" scrollable scrollHeight="flex" class="p-datatable-sm table-jobs-self" >
+                    <DataTable :value="data" scrollable scrollHeight="400px" class="p-datatable-sm table-jobs-self"
+                        :virtualScrollerOptions="{
+                                itemSize: 20,
+                                lazy: true,
+                                showLoader: true,
+                                loading: isLoadingData,
+                                onLazyLoad: onLazyLoad
+                            }">
                         <Column header="" class="col-image">
                             <template #body="slotProps">
                                 <img :src="slotProps.data.image" style="width: 120px; height: 80px;" />
                             </template>
                         </Column>
-                        <Column header="Klusje" class="col-job" >
+                        <Column header="Klusje" class="col-job">
                             <template #body="slotProps">
                                 <div class="column description">
                                     <h2>{{ slotProps.data.name }}</h2>
@@ -113,7 +63,7 @@ const jobs = ref([
                         <Column field="category" header="Category" style="width: 120px" class="col-category"></Column>
                         <Column header="Labels" class="col-label flex flex-column gap-1 justify-content-center">
                             <template #body="slotProps">
-                                <Tag severity="info" :value="'€ ' + slotProps.data.price "></Tag>
+                                <Tag severity="info" :value="'€ ' + slotProps.data.price"></Tag>
                                 <Tag :severity="getSeverity(slotProps.data.status)" :value="slotProps.data.status"></Tag>
                             </template>
                         </Column>
@@ -135,6 +85,7 @@ const jobs = ref([
     height: 100%;
     padding: 0.25rem 0;
 }
+
 .description p,
 .description h2 {
     margin: 0;
@@ -145,6 +96,7 @@ const jobs = ref([
     height: 28px;
     overflow: hidden;
 }
+
 .description p {
     max-height: 56px;
     overflow: hidden;
@@ -161,7 +113,8 @@ const jobs = ref([
     min-width: calc(300px + 1rem);
 }
 
-.col-category, .col-label {
+.col-category,
+.col-label {
     min-width: calc(120px + 1rem);
     max-width: calc(120px + 1rem);
 }
