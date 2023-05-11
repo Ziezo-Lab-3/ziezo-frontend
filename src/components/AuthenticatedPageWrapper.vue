cccb74e4d3d075fc11608d16328c26318f6bcd18
GilianS <r0885351@student.thomasmore.be>
2023-05-11 10:53:42 +0200

    refactor: update page routes to better facilitate authentication

---
+++ src/components/AuthenticatedPageWrapper.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import { RouterView } from 'vue-router';
+import Navigation from './Navigation.vue';
+import Sidebar from 'primevue/sidebar';
+
+const menuOpen = ref(false);
+const content = ref(null);
+
+const contentScroll = ref(0);
+
+onMounted(() => {
+    content.value.addEventListener('scroll', () => {
+        const newScroll = content.value.scrollTop;
+        if (newScroll > contentScroll.value) {
+            document.getElementById('menu_toggle').classList.add('hidden');
+        } else {
+            document.getElementById('menu_toggle').classList.remove('hidden');
+        }
+        contentScroll.value = newScroll;
+    })
+});
+</script>
+<template>
+    <div id="nav_sidebar">
+        <Navigation></Navigation>
+    </div>
+    <div id="content" ref="content">
+        <RouterView></RouterView>
+    </div>
+    <div id="nav_mobile">
+        <Sidebar v-model:visible="menuOpen" position="bottom" class="p-sidebar-md">
+            <template #header>
+                <div class="nav-logo" >
+                    <img src="/images/Logo.svg" alt="Ziezo Logo">
+                </div>
+            </template>
+            <Navigation></Navigation>
+        </Sidebar>
+        <div id="menu_toggle" @click="() => menuOpen = !menuOpen"><i class="pi pi-bars" style="font-size: 1.5rem"></i></div>
+    </div>
+</template>
+<style scoped>
+.nav-logo {
+    width: calc(100vw - 4rem);
+    display: flex;
+    justify-content: center;
+    padding-left: 2rem;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,93 +1,78 @@
 import { createWebHistory, createRouter } from "vue-router";
 
-import HomeView from "./views/HomeView.vue";
-import MessageView from "./views/MessageView.vue";
-import JobView from "./views/JobView.vue";
-import ProfileView from "./views/ProfileView.vue";
+import AuthenticatedPageWrapper from "./components/AuthenticatedPageWrapper.vue";
 
-import ContactView from "./views/ContactView.vue";
-import SettingsView from "./views/SettingsView.vue";
+import HomeView from "./views/app/HomeView.vue";
+import MessageView from "./views/app/MessageView.vue";
+import JobView from "./views/app/JobView.vue";
+import ProfileView from "./views/app/ProfileView.vue";
+import ContactView from "./views/app/ContactView.vue";
+import SettingsView from "./views/app/SettingsView.vue";
+
+import LoginView from "./views/auth/LoginView.vue";
+import SignupView from "./views/auth/SignupView.vue";
 
 import NotFoundView from "./views/NotFoundView.vue";
 import TestView from "./views/TestView.vue";
-import LoginView from "./views/LoginView.vue";
-import ForgotPasswordView from "./views/ForgotPasswordView.vue";
-import SignupView from "./views/SignupView.vue";
-import NewPasswordView from "./views/NewPasswordView.vue";
-import NewPasswordSuccesView from "./views/NewPasswordSuccesView.vue";
-import CreateProfileView from "./views/CreateProfileView.vue";
 
 const routes = [
     {
-        path: "/",
-        name: "Home",
-        component: HomeView,
+        path: "/app",
+        name: "Verifieer Gebruiker",
+        component: AuthenticatedPageWrapper,
+        children: [
+            {
+                path: "/",
+                name: "Home",
+                component: HomeView,
+            },
+            {
+                path: "/message",
+                name: "Berichten",
+                component: MessageView,
+            },
+            {
+                path: "/jobs",
+                name: "Mijn Klusjes",
+                component: JobView,
+            },
+            {
+                path: "/profile",
+                name: "Mijn Profiel",
+                component: ProfileView,
+            },
+            {
+                path: "/test",
+                name: "Test",
+                component: TestView,
+            },
+            {
+                path: "/contact",
+                name: "Contact",
+                component: ContactView,
+            },
+            {
+                path: "/settings",
+                name: "Instellingen",
+                component: SettingsView,
+            },
+        ],
     },
     {
-        path: "/message",
-        name: "Berichten",
-        component: MessageView,
+        path: "/login",
+        name: "Login",
+        component: LoginView
     },
     {
-        path: "/jobs",
-        name: "Mijn Klusjes",
-        component: JobView,
-    },
-    {
-        path: "/profile",
-        name: "Mijn Profiel",
-        component: ProfileView,
-    },
-    {
-        path: "/test",
-        name: "Test",
-        component: TestView,
-    },
-    {
-        path: "/contact",
-        name: "Contact",
-        component: ContactView,
-    },
-    {
-        path: "/settings",
-        name: "Instellingen",
-        component: SettingsView,
+        path: "/signup",
+        name: "Nieuwe Gebruiker",
+        component: SignupView
     },
     {
         path: "/:pathMatch(.*)*",
         name: "Pagina Niet Gevonden",
         component: NotFoundView
     },
-    {
-        path: "/Login",
-        name: "Login",
-        component: LoginView
-    },
-    {
-        path: "/ForgotPassword",
-        name: "Wachtwoord vergeten",
-        component: ForgotPasswordView
-    },
-    {
-        path: "/Aanmelden",
-        name: "Aanmelden",
-        component: SignupView
-    },
-    {
-        path: "/NewPassword",
-        name: "Nieuw Wachtwoord",
-        component: NewPasswordView
-    },
-    {
-        path: "/NewPasswordSucces",
-        name: "Nieuw Wachtwoord geaccepteerd",
-        component: NewPasswordSuccesView
-    },
-    {
-        path: "/CreateProfile",
-        name: "Profiel aanmaken",
-        component: CreateProfileView
-    }
 ];
 
 const router = createRouter({
