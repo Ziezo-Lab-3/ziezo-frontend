07ea16b7d333396d01f7dc19092e3c6d42a8ffde
GilianS <r0885351@student.thomasmore.be>
2023-05-11 15:06:35 +0200

    feat: check token validity or route to login

--- src/js/decodeJWT.js
+++ src/js/decodeJWT.js
@@ -1,3 +1,9 @@
+/**
+ * Decodes a JWT token and returns the decoded JWT
+ * @param {string} token - JWT token
+ * @returns {{ exp: number, iat: number, id: string }} decoded JWT
+ * @see https://stackoverflow.com/a/38552302/12399621
+ */
 const decodeJWT = (token) => {
     const base64Url = token.split(".")[1];
     const base64 = base64Url.replace("-", "+").replace("_", "/");
