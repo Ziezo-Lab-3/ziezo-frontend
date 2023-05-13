4bcd14892a1eca738c727a532115f11976ae6cf7
GilianS <r0885351@student.thomasmore.be>
2023-05-13 18:25:45 +0200

    fix: login page width

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -20,102 +20,80 @@ const signin = () => {
             password: password.value,
         }),
     })
-    // Converteer de response naar JSON
-    .then((response) => response.json())
+        // Converteer de response naar JSON
+        .then((response) => response.json())
 
-     // Verwerk de response
-    .then((response) => {
-        if (response.status === "success") {
-            // Sla de token op in de localStorage
-            localStorage.setItem("token", response.data.accessToken);
-            // Reset de velden
-            message.value = "";
-            router.push("/");
-        }
-        else if (response.status === "fail") {
-            message.value = response.message;
-            console.log("error:" + response.message);
-        }
-    });
-    
+        // Verwerk de response
+        .then((response) => {
+            if (response.status === "success") {
+                // Sla de token op in de localStorage
+                localStorage.setItem("token", response.data.accessToken);
+                // Reset de velden
+                message.value = "";
+                router.push("/");
+            }
+            else if (response.status === "fail") {
+                message.value = response.message;
+                console.log("error:" + response.message);
+            }
+        });
+
 };
 
 
 </script>
 <template>
-    <Card class="p-m-4 p-major">
-        <template #header>
-            <div class="Login-logo">
-                <img src="/images/Logo.svg" alt="Ziezo Logo">
-            </div>
-        </template>
-        <template #title>
-            Inloggen
-        </template>
-        <template #content>
-            <div class="p-fluid">
-                <div class="p-field">
-                    <label for="email">Email</label>
-                    <InputText id="email" v-model="email" />
+    <div class="w-screen h-screen p-2 flex justify-content-center align-items-center">
+        <Card class="max-w-full w-20rem h-fit">
+            <template #header>
+                <div class="pb-2 pt-4 px-6">
+                    <img src="/images/Logo.svg" alt="Ziezo Logo">
                 </div>
-                <div class="p-field">
-                    <label for="password">Wachtwoord</label>
-                    <Password id="password" v-model="password" />
+            </template>
+            <template #title>
+                Inloggen
+            </template>
+            <template #content>
+                <div class="p-fluid">
+                    <div class="labelInput">
+                        <label for="email">Email</label>
+                        <InputText id="email" v-model="email" />
+                    </div>
+                    <div class="labelInput">
+                        <label for="password">Wachtwoord</label>
+                        <Password id="password" :feedback="false" v-model="password" />
+                    </div>
+                    <div style="color: var(--danger)" v-if="message !== ''">{{ message }}</div>
+                    <div class="labelInput">
+                        <Button label="Inloggen" @click="signin" />
+                    </div>
+                    <div class="labelInput">
+                        <RouterLink to="/Aanmelden">
+                            <Button class="p-button-secondary" label="Account aanmaken" />
+                        </RouterLink>
+                    </div>
+                    <!--forgot password-->
+                    <div class="labelInput">
+                        <!--div on click-->
+                        <RouterLink to="/ForgotPassword">
+                            <div class="link-simple">Wachtwoord vergeten?</div>
+                        </RouterLink>
+                    </div>
                 </div>
-                <div style="color: var(--danger)" v-if="message !== ''">{{ message }}</div>
-                <div class="p-field">
-                    <Button label="Inloggen" @click="signin" />
-                </div>
-                <div class="p-field">
-                    <RouterLink to="/Aanmelden">
-                        <Button class="p-button-secondary" label="Account aanmaken"/>
-                    </RouterLink>
-                </div>
-                <!--forgot password-->
-                <div class="p-field">
-                <!--div on click-->
-                    <RouterLink to="/ForgotPassword">
-                        <div class="link-simple">Wachtwoord vergeten?</div> 
-                    </RouterLink>
-                </div>
-            </div>
-        </template>
-    </Card>
+            </template>
+        </Card>
+    </div>
 </template>
 
 <style scoped>
-.p-field{
-    margin-bottom: 0.75rem;
-}
-.Login-logo {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    padding-left: 2rem;
-    padding: 32px 90px;
-}
-.p-major{
-    width: 100%;
-    max-width: 400px;
-}
-.link-simple{
+
+.link-simple {
     text-decoration: underline;
-    color: #000;
-    background-color: transparent;
-    border: none;
-    padding: 0;
-    font: inherit;
-    cursor: pointer;
-    outline: inherit;
+    color: var(--secondary);
     text-align: center;
 }
-.link-simple:hover{
-    color: #000;
-    background-color: transparent;
-    border: none;
-    padding: 0;
-    font: inherit;
-    cursor: pointer;
-    outline: inherit;
+
+.link-simple:hover {
+    color: var(--secondary-dark-1);
 }
 </style>
