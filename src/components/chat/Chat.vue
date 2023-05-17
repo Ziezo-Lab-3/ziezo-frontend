00a763ccb09550e96a227d917cae74c23f68a3fd
GilianS <r0885351@student.thomasmore.be>
2023-05-17 15:15:24 +0200

    feat: show messages with proper stacking

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -1,8 +1,39 @@
 <script setup>
 import { reactive, onMounted } from 'vue';
+import decodeJWT from '../../js/decodeJWT';
+import Message from './Message.vue';
 
 const state = reactive({
-    messages: [
+    decodedJWT: null,
+    messages: [],
+    users: [],
+    initialized: false,
+    isLoadingData: false,
+    message: "",
+});
+
+const getMessageFormat = (message) => {
+    // If the message is null, return null
+    const index = state.messages.findIndex((value) => value.id === message.id);
+    if (message === null) return;
+    const previous = state.messages[index - 1];
+    const next = state.messages[index + 1];
+    console.log(index)
+
+    // Check if the message is the first or last message from the sender
+    const isTop = !previous || (previous && previous.sender !== message.sender);
+    const isBottom = next && next.sender !== message.sender;
+    
+    // If the message is the first or last message in the list, return the correct format
+    if (isTop && isBottom) return "single";
+    if (isTop) return "top";
+    if (isBottom) return "bottom";
+    return "middle";
+}
+
+onMounted(() => {
+    state.decodedJWT = decodeJWT(localStorage.getItem("token"));
+    state.messages = [
         {
             id: 1,
             message: "Hallo",
@@ -15,45 +46,45 @@ const state = reactive({
             message: "Hey",
             sender: "64009e396dd8fec7e85aa8b4",
             chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T12:00:00.000Z",
+            createdAt: "2021-10-01T12:02:00.000Z",
         },
         {
             id: 3,
             message: "Bruh 1",
             sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
             chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T12:00:00.000Z",
+            createdAt: "2021-10-01T13:05:00.000Z",
         },
         {
             id: 4,
             message: "Bruh 2",
             sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
             chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T12:00:00.000Z",
+            createdAt: "2021-10-01T13:05:30.000Z",
         },
         {
             id: 5,
             message: "Bruh 3",
             sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
             chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T12:00:00.000Z",
+            createdAt: "2021-10-01T13:05:40.000Z",
         },
         {
             id: 6,
             message: "Ik heb best wat ervaring in het klussen. Ik kan je helpen met het ophangen van een schilderij, het in elkaar zetten van een kast of het vervangen van een lamp. Ik heb een auto dus ik kan ook helpen met verhuizen.",
             sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
             chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T12:00:00.000Z",
+            createdAt: "2021-10-03T12:00:00.000Z",
         },
         {
             id: 7,
             message: "Cool",
             sender: "64009e396dd8fec7e85aa8b4",
             chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T12:00:00.000Z",
+            createdAt: "2021-10-03T14:00:00.000Z",
         },
-    ],
-    users: [
+    ];
+    state.users = [
         {
             id: "aaaaaaaaaaaaaaaaaaaaaaaa",
             name: "Kaitlyn Ullrich",
@@ -64,28 +95,29 @@ const state = reactive({
             name: "Test User",
             avatar: "https://randomuser.me/api/portraits/men/18.jpg"
         }
-    ],
-    initialized: false,
-    isLoadingData: false,
-    message: "",
+    ]
+    state.initialized = true;
 });
 
 </script>
 <template>
 <div class="chat__wrapper">
-    <div class="chat__header"></div>
+    <div class="chat__header"><h2>Chat</h2></div>
     <div class="chat__messages">
         <div v-if="!state.initialized">
             Loading...
         </div>
         <div v-else-if="state.messages.length === 0">
-            <p>Geen berichten</p>
+            <p class="chat__empty-message">Stuur {{ state.users.length === 2 ? state.users.find(value => value.id !== state.decodedJWT.id).name + " " : ""}} een bericht en start de conversatie!</p>
         </div>
-        <div v-else>
-            <div v-for="message in state.messages" :key="message.id" class="chat__message">
-                
-            </div>
-        </div>
+                <Message v-for="message in state.messages" :key="message.id"
+                    :avatar="state.users.find(value => value.id === message.sender).avatar"
+                    :text="message.message" 
+                    :self="message.sender === state.decodedJWT.id" 
+                    :date="message.createdAt"
+                    :name="state.users.find(value => value.id === message.sender).name"
+                    :format="getMessageFormat(message)"
+                />
     </div>
     <div class="chat__input">
         <InputText v-model="state.message" placeholder="Aa" />
@@ -97,6 +129,8 @@ const state = reactive({
 .chat__wrapper {
     display: grid;
     grid-template-rows: auto 1fr auto;
+    max-height: 100%;
+    min-height: 100%;
 }
 .chat__input {
     display: grid;
@@ -104,5 +138,17 @@ const state = reactive({
     grid-template-columns: 1fr auto;
     grid-gap: .5rem;
     padding: .25rem;
+}
+
+.chat__empty-message {
+    text-align: center;
+    margin: 0;
+    padding: 1rem;
+    color: var(--secondary);
+}
+
+.chat__messages {
+    overflow-y: auto;
+    padding: 1rem 0;
 }
 </style>
