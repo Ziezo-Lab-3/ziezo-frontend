b70a363408e141578b2f9a2e9ec9612a3dbf6e23
GilianS <r0885351@student.thomasmore.be>
2023-05-21 19:40:06 +0200

    feat: succesfull call to message endpoint

--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -2,31 +2,29 @@
 import { reactive, onMounted, watch } from 'vue';
 import decodeJWT from '../../js/decodeJWT';
 import Message from './Message.vue';
+import { getMessages, postMessage } from '../../api/message';
 
 const props = defineProps({
-    /**
-     * The chat group to display the messages from
-     * @type {{
-            _id: String,
-            name: String,
-            members: Array,
-            avatar: String | null,
-            lastMessage: String | null,
-            unreadMessages: Number | null,
-            createdAt: String,
-            updatedAt: String | null,
-        }}
-     */
     chatGroup: {
         type: Object,
         required: true,
     },
 });
-
+/**
+  * The chat group to display the messages from
+  * @type {{
+  * decodedJWT: { id: String, iat: Number, exp: Number } | null,
+  * messages: Array,
+  * chatGroup: { _id: String, name: String, members: Array, avatar: String | null, lastMessage: String | null, unreadMessages: Number | null, createdAt: String, updatedAt: String | null, } | null,
+  * initialized: Boolean,
+  * isLoadingData: Boolean,
+  * message: String,
+  * }}
+*/
 const state = reactive({
     decodedJWT: null,
     messages: [],
-    users: [],
+    chatGroup: null,
     initialized: false,
     isLoadingData: false,
     message: "",
@@ -50,76 +48,29 @@ const getMessageFormat = (message) => {
     return "middle";
 }
 
+const loadMessages = async (first, last) => {
+    state.initialized = false;
+    state.isLoadingData = true;
+    const messages = await getMessages(localStorage.getItem("token"), {
+        first,
+        last,
+        filter: {
+            chatGroup: state.chatGroup._id
+        }
+    });
+    state.messages = messages.data;
+    state.initialized = true;
+    state.isLoadingData = false;
+}
+
 // Handle scroll behavior of chat__messages
 const scrollChat = () => {
     const chatMessages = document.querySelector(".chat__messages");
     chatMessages.scrollTop = chatMessages.scrollHeight;
 }
+
 onMounted(() => {
     state.decodedJWT = decodeJWT(localStorage.getItem("token"));
-    state.messages = [
-        {
-            id: 1,
-            message: "Hallo",
-            sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T12:00:00.000Z",
-        },
-        {
-            id: 2,
-            message: "Hey",
-            sender: "64009e396dd8fec7e85aa8b4",
-            chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T12:02:00.000Z",
-        },
-        {
-            id: 3,
-            message: "Bruh 1",
-            sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T13:05:00.000Z",
-        },
-        {
-            id: 4,
-            message: "Bruh 2",
-            sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T13:05:30.000Z",
-        },
-        {
-            id: 5,
-            message: "Bruh 3",
-            sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-01T13:05:40.000Z",
-        },
-        {
-            id: 6,
-            message: "Ik heb best wat ervaring in het klussen. Ik kan je helpen met het ophangen van een schilderij, het in elkaar zetten van een kast of het vervangen van een lamp. Ik heb een auto dus ik kan ook helpen met verhuizen.",
-            sender: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-03T12:00:00.000Z",
-        },
-        {
-            id: 7,
-            message: "Cool",
-            sender: "64009e396dd8fec7e85aa8b4",
-            chatGroup: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            createdAt: "2021-10-03T14:00:00.000Z",
-        },
-    ];
-    state.users = [
-        {
-            id: "aaaaaaaaaaaaaaaaaaaaaaaa",
-            name: "Kaitlyn Ullrich",
-            avatar: "https://randomuser.me/api/portraits/women/51.jpg"
-        },
-        {
-            id: "64009e396dd8fec7e85aa8b4",
-            name: "Test User",
-            avatar: "https://randomuser.me/api/portraits/men/18.jpg"
-        }
-    ];
     state.initialized = true;
 });
 
@@ -127,6 +78,14 @@ watch(() => state.messages, () => {
     setTimeout(() => {
         scrollChat();
     }, 500);
+});
+
+watch(() => props.chatGroup, async () => {
+    state.messages = [];
+    state.chatGroup = props.chatGroup;
+    console.log(props.chatGroup);
+    await loadMessages(0, 10);
+    scrollChat();
 });
 
 </script>
@@ -137,15 +96,15 @@ watch(() => state.messages, () => {
         <div v-if="!state.initialized">
             Loading...
         </div>
-        <div v-else-if="state.messages.length === 0">
-            <p class="chat__empty-message">Stuur {{ state.users.length === 2 ? state.users.find(value => value.id !== state.decodedJWT.id).name + " " : ""}} een bericht en start de conversatie!</p>
+        <div v-else-if="state.messages?.length === 0">
+            <p class="chat__empty-message">Stuur een bericht en start de conversatie!</p>
         </div>
                 <Message v-for="message in state.messages" :key="message.id"
-                    :avatar="state.users.find(value => value.id === message.sender).avatar"
+                    :avatar="state.chatGroup.members.find(value => value.id === message.sender).avatar"
                     :text="message.message" 
                     :self="message.sender === state.decodedJWT.id" 
                     :date="message.createdAt"
-                    :name="state.users.find(value => value.id === message.sender).name"
+                    :name="state.chatGroup.members.find(value => value.id === message.sender).name"
                     :format="getMessageFormat(message)"
                 />
     </div>
--- src/js/parseParams.js
+++ src/js/parseParams.js
@@ -2,7 +2,10 @@ export const parseParams = (params) => {
     let suffix = "";
     //Get all the keys and values from the filter object, and append them to the url.
     for (const [key, value] of Object.entries(params)) {
-        suffix += `${key}=${value}&`;
+        if (typeof value === "object") {
+            suffix += `${key}=${JSON.stringify(value)}&`;
+        }
+        else suffix += `${key}=${value}&`;
     }
     //Remove the last & from the url.
     return suffix.slice(0, -1);
